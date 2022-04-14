fdff0b56b7e1291ce9d9f999248f13f6fc97b3a0
Sumanth <Sumanth@DESKTOP-R6Q0H6T>
2022-04-14 18:38:54 +0530

    commit browser url

--- SDET33JENKINS/src/test/java/com/crm/test/TestCase_01_Test.java
+++ SDET33JENKINS/src/test/java/com/crm/test/TestCase_01_Test.java
@@ -9,6 +9,10 @@ public class TestCase_01_Test {
 	
 	@Test
 	public void test1() {
+		String browser=System.getProperty("BROWSER");
+		String url=System.getProperty("URL");
+		Reporter.log(browser,true);
+		Reporter.log(url,true);
 		Reporter.log("hi",true);
 	}
 
